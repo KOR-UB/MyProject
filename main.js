23938210bfe3197e717be85b0f00ec75f43a4efb
= <yyb2535@gmail.com>
2020-04-24 22:49:33 +0900

    push

--- main.js
+++ main.js
@@ -4,7 +4,7 @@ var url = require("url");
 
 var numstr = /^[^]\d/;
 
-function templateHTML(title, list, body, css) {
+function templateHTML(title, list, body, css, listlength) {
     return `
     <!DOCTYPE html>
     <html lang="ko">
@@ -42,7 +42,7 @@ function templateHTML(title, list, body, css) {
                     <i class="devicon-javascript-plain"></i>
                     <div class="titleBx">
                         <h2>JavaScript</h2>
-                        <p><em class="titlenum">1 </em>lessons</p> 
+                        <p><em class="titlenum">${listlength} </em>lessons</p> 
                     </div>
                 </div>
                 <div class="content">
@@ -50,7 +50,47 @@ function templateHTML(title, list, body, css) {
                 </div>
             </div>
         </div>
-        ${body}
+    </body>
+    </html>
+    `;
+}
+function templateHTML2(title, body, css) {
+    return `
+    <!DOCTYPE html>
+    <html lang="ko">
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>poiemaweb Clone - ${title}</title>
+        <link href="https://fonts.googleapis.com/css2?family=Lobster&display=swap" rel="stylesheet">
+        <link rel="stylesheet" href="https://cdn.rawgit.com/konpa/devicon/df6431e323547add1b4cf45992913f15286456d3/devicon.min.css">
+        <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
+        <link href="https://fonts.googleapis.com/css2?family=Montserrat:wght@800&display=swap" rel="stylesheet">
+        <style>
+           ${css}
+        </style>
+    </head>
+    <body>
+        <header>
+            <h1>${title}</h1>
+        </header>
+        <div class="wraper" id="wraper">
+            <div class="sticky">
+                <h2><a href="#"><i class="devicon-javascript-plain"></i>JAVASCRIPT</a></h2>
+                <button class="listtoggle"><i class="fa fa-list-ol"></i></button>
+            </div>
+            <div class="max1140">
+                ${body}
+            </div>
+        </div>
+        <script>
+            const listtoggle = document.querySelector(".listtoggle");
+            listtoggle.addEventListener("click",function()
+            {
+                const hiddenlist = document.querySelector(".hiddenlist-group");
+                hiddenlist.classList.toggle("active");
+            });
+        </script>
     </body>
     </html>
     `;
@@ -80,7 +120,8 @@ var app = http.createServer(function (request, response) {
                     var title = '';
                     var description = '';
                     var list = templateList(filelist);
-                    var template = templateHTML(title, list, `<h2>${title}</h2>${description}`, indexcss);
+                    var listLength = filelist.length;
+                    var template = templateHTML(title, list, `<h2>${title}</h2>${description}`, indexcss, listLength);
                     response.writeHead(200);
                     response.end(template);
                 });
@@ -88,11 +129,11 @@ var app = http.createServer(function (request, response) {
         } else {
             fs.readdir('./data', function (error, filelist) {
                 fs.readFile(`data/${queryData.id}`, 'utf8', function (err, description) {
-                    fs.readFile(`indexcss`, 'utf8', function (err, indexcss) {
+                    fs.readFile(`indexcss2`, 'utf8', function (err, indexcss2) {
                         var title = queryData.id;
                         var list = templateList(filelist);
                         title = title.replace(numstr, "");
-                        var template = templateHTML(title, list, `<h2 class="js-title">${title}</h2><p class="js-description">${description}</p>`, indexcss);
+                        var template = templateHTML2(title, description, indexcss2);
                         response.writeHead(200);
                         response.end(template);
                     });
---
+++ test.js
@@ -0,0 +1,22 @@
+// var str = "abcdef";
+// var a = "abc" + "def";
+// var x = "abc"[2] + "def";
+// var t = "abc" + "def"[2];
+// var y = "abc"[2] + "def"[2];
+// var z = "abcdef"[2];
+// console.log(str);
+// console.log(a);
+// console.log(x);
+// console.log(t);
+// console.log(y);
+// console.log(z);
+// var q = "1";
+// q *= 10;
+// console.log(q);
+var x = 1;
+var result = "";
+
+
+
+result = x % 2 ? "홀수" : "짝수";
+console.log(result);
