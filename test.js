1ab36d0bb302127e4964d67edeff2aac14ee3639
= <yyb2535@gmail.com>
2020-04-30 21:21:56 +0900

    push

---
+++ test.js
@@ -0,0 +1,63 @@
+// const obj = 
+// {
+//     name: "동욱"[0] + "유비"[0],
+//     sayHi(x)
+//     {
+//         console.log(x);
+//         return x;
+//     },
+
+// };
+// const _name = "동욱" + "유비"
+
+// console.log(obj.name[1]); 
+// console.log(_name[3]);
+
+// function add( ... arg)
+// {
+//     console.log(arg);
+//     var result = 0;
+//     for(let i = 0; i < arguments.length; i++)
+//     {
+//         result += arguments[i];
+//     }
+//     return result;
+// }
+// console.log(add(2,5,10, 100, 20, 11, 9));
+
+// function Test(p, o)
+// {
+//     p += 100;
+//     o.name = 'Kim';
+//     console.log(p, o)
+// }
+// var num = 100;
+// var person =  { name: "lee" };
+
+// Test(num, person);
+
+// console.log(num);
+// console.log(person);
+
+// function test()
+// {
+//     x = x + a;
+//     console.log(x)
+// }
+// let x = 1;
+// let a = 10;
+// test()
+// function outer() {
+//   var x = 1;
+
+//   // 중첩 함수
+//   function inner() {
+//     var y = 2;
+//     // 외부 함수의 변수를 참조할 수 있다.
+//     console.log(x + y); // 3
+//   }
+
+//   inner();
+// }
+
+// outer();
